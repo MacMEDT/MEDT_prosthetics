a031174696a3ee310521fc9383d096aeb130df0d
kavash11 <80080693+kavash11@users.noreply.github.com>
2023-11-17 14:39:39 -0500

    5 motors and 5 FSRs

--- Haptics/Haptics.ino
+++ Haptics/Haptics.ino
@@ -1,6 +1,9 @@
-#define fsrpin1 A4
-#define fsrpin2 A2
-#define n 2 // number of force sensors added
+#define fsrpin1 A0
+#define fsrpin2 A1
+#define fsrpin3 A2
+#define fsrpin4 A3
+#define fsrpin5 A4
+#define n 5 // number of force sensors added
 
   //int n =2; // number of force sensors
   int fsr[n]; // array for storing force sensor readings
@@ -13,6 +16,9 @@ void setup() {
   Serial.begin(9600);
   pinMode(5, OUTPUT); // vibration motor 1
   pinMode(6, OUTPUT); // vibration motor 2
+  pinMode(3, OUTPUT); // vibration motor 2
+  pinMode(10, OUTPUT); // vibration motor 2
+  pinMode(11, OUTPUT); // vibration motor 2
   for (int i = 0; i<n; i++){
     fsr[i] = 0;
   }
@@ -21,42 +27,38 @@ void setup() {
 void loop() {
   fsr[0] = analogRead(fsrpin1);
   fsr[1] = analogRead(fsrpin2);
+  fsr[2] = analogRead(fsrpin3);
+  fsr[3] = analogRead(fsrpin4);
+  fsr[4] = analogRead(fsrpin5);      
 
   int intensity[n];
   for (int i = 0; i<n; i++){
-    Serial.print("force sensor ");
-    Serial.print(i+1);
-    Serial.print(": ");
-    Serial.println(fsr[i]);
 
+    Serial.println(fsr[2]);
+    Serial.println();
 
     intensity[i] = (int)((fsr[i] / 1050.0)*255.0); //multiply actual force by a factor of the possible max force
 
     if (lastreading[i] == 0 && fsr[i] >= 20){
       intensity[i] = 200; //provide a higher amplitude pulse at the beginning of touch
-    } /*else if (intensity[i] < 100 && intensity[i] >0){
-      intensity[i] = 110; //sets minimum for PWM to 100 because intensity below ~70 doesn't actually produce vibration.
-    }*/
+    } 
     if (intensity[i] < 0){ //remove negative intensities
       intensity[i] = 0;
     }
 
     intensity[i] = intensity[i] - (int)intensity_mod[i]; //applying intensity modification factor
     if (intensity[i] < 70 && intensity[i] >20){
-      intensity[i] = 80;
+      intensity[i] = 80; //sets minimum for PWM to 80 because intensity below ~70 doesn't actually produce vibration.
     } 
-
-    Serial.print("intensity ");
-    Serial.print(i+1);
-    Serial.print(": ");
-    Serial.println(intensity[i]);
-    Serial.println();
   }
 
 
 
   analogWrite(5, intensity[0]); // write the intensity to vibration motors
   analogWrite(6, intensity[1]);
+  analogWrite(3, intensity[2]);
+  analogWrite(10, intensity[3]);
+  analogWrite(11, intensity[4]);
 
   for(int i = 0; i<n; i++){ //deducing the intensity modification factor
     if (fsr[i] <10){
