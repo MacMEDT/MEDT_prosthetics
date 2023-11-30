be96a54f83e38abcd122212ce8da01001c095526
kavash11 <80080693+kavash11@users.noreply.github.com>
2023-11-30 16:57:01 -0500

    forcevibration  tapers off during sustained holding

--- Haptics/Haptics.ino
+++ Haptics/Haptics.ino
@@ -9,7 +9,7 @@
   int fsr[n]; // array for storing force sensor readings
   int lastreading[n];
 
-  float intensity_mod[n];
+  int intensity_mod[n];
   int timer[n];
 
 void setup() {
@@ -34,44 +34,62 @@ void loop() {
   int intensity[n];
   for (int i = 0; i<n; i++){
 
-    Serial.println(fsr[2]);
-    Serial.println();
+   /* Serial.print(fsr[0]);
+    Serial.print(", ");
+    Serial.print(fsr[1]);
+    Serial.print(", ");
+    Serial.print(fsr[2]);
+    Serial.print(", ");
+    Serial.print(fsr[3]);
+    Serial.print(", ");
+    Serial.print(fsr[4]);
+    Serial.println();*/
 
     intensity[i] = (int)((fsr[i] / 1050.0)*255.0); //multiply actual force by a factor of the possible max force
 
     if (lastreading[i] == 0 && fsr[i] >= 20){
       intensity[i] = 200; //provide a higher amplitude pulse at the beginning of touch
     } 
+      Serial.print(intensity[0]);
+      Serial.print(", ");
+      Serial.print(intensity_mod[0]);
+      Serial.println();
+   intensity[i] = (intensity[i] - intensity_mod[i]); //applying intensity modification factor
     if (intensity[i] < 0){ //remove negative intensities
       intensity[i] = 0;
     }
-
-    intensity[i] = intensity[i] - (int)intensity_mod[i]; //applying intensity modification factor
-    if (intensity[i] < 70 && intensity[i] >20){
+    /*if (intensity[i] < 70 && intensity[i] >20){
       intensity[i] = 80; //sets minimum for PWM to 80 because intensity below ~70 doesn't actually produce vibration.
-    } 
+    }*/
   }
 
 
 
-  analogWrite(5, intensity[0]); // write the intensity to vibration motors
-  analogWrite(6, intensity[1]);
-  analogWrite(3, intensity[2]);
-  analogWrite(10, intensity[3]);
-  analogWrite(11, intensity[4]);
+
 
   for(int i = 0; i<n; i++){ //deducing the intensity modification factor
     if (fsr[i] <10){
-      lastreading[i] = 0; // checks if there's no force, which resets the modifications and flags that the last reading was 0 
+      lastreading[i] = 250; // checks if there's no force, which resets the modifications and flags that the last reading was 0 
       timer[i] = 0;
       intensity_mod[i] = 0;
     } else {
       lastreading[i] = 1; //sustained pressure will result in the intensity tapering off. 
       timer[i]+=50;
-      if (timer[i] > 5000){
-        intensity_mod[i]+=0.25; //increase the intensity modification factor over time
+      /*Serial.print(timer[i]);
+      Serial.print(", ");
+      Serial.println(fsr[0]);*/
+
+      if (timer[i] > 500 and fsr[0]>400 or fsr[1]>400 or fsr[4]>400){
+      intensity_mod[i]+=10; //increase the intensity modification factor over time
+      
       }
     }
+  //intensity[i] = abs(intensity[i] - intensity_mod[i]);
+  analogWrite(5, intensity[0]); // write the intensity to vibration motors
+  analogWrite(6, intensity[1]);
+  analogWrite(3, intensity[2]);
+  analogWrite(10, intensity[3]);
+  analogWrite(11, intensity[4]);
   }
 
   delay(200);
