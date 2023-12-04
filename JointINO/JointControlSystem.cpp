5ee09b6d214d5970b2be02b9edbfd83bac30d190
ecmcelheran <ecmcelheran15@gmail.com>
2023-12-04 17:54:45 -0500

    ylim([-150 200]) xlim([0 185]) Update to control system

--- JointINO/JointControlSystem.cpp
+++ JointINO/JointControlSystem.cpp
@@ -5,70 +5,20 @@ JointControlSystem::JointControlSystem()
     joints[THUMB] = ThumbJoint(THUMB);
     joints[INDEX] = IndexJoint(INDEX);
     joints[OTHER] = OtherDigJoint(OTHER);
-    joints[WRIST_FLEX] = WristFlexJoint(WRIST_FLEX);
-    joints[WRIST_ROT] = WristRotJoint(WRIST_ROT);
 }
 
 void JointControlSystem::processSignals(float signal[]){
-
+    //To Do later
 };
+
+void JointControlSystem::setGrip(float emg){
+    //values from emg_real.txt generally 0.2-0.5
+    if (emg>0 && emg<0.35){grip = GRIP_FLAT;} 
+    else if (emg>=0.35){grip = GRIP_FIST;} 
+    else{grip = GRIP_NONE;}
+}
 
 int JointControlSystem::getGrip()
 {
-    // get angles of each joint
-    float thumbAngle = joints[THUMB].getAngle();
-    float indexAngle = joints[INDEX].getAngle();
-    float otherDigitsAngle = joints[OTHER].getAngle();
-    float wristFlexAngle = joints[WRIST_FLEX].getAngle();
-    float wristRotAngle = joints[WRIST_ROT].getAngle();
-
-    // define angle thresholds
-    auto isFlat = [](float angle) -> bool
-    {
-        return angle == 0;
-    };
-    auto isCurled = [](float angle) -> bool
-    {
-        return 35 <= angle <= 40;
-    };
-    auto isClosed = [](float angle) -> bool
-    {
-        return 75 <= angle <= 80;
-    };
-
-    // define thresholds for each grip
-    if (isFlat(wristRotAngle))
-    {
-        if (isFlat(wristFlexAngle))
-        {
-            if (isFlat(thumbAngle) && isClosed(indexAngle) && isClosed(otherDigitsAngle))
-            {
-                return GRIP_POWER;
-            }
-            if (isClosed(thumbAngle) && isClosed(indexAngle) && isClosed(otherDigitsAngle))
-            {
-                return GRIP_HOOK;
-            }
-            if (isFlat(thumbAngle) && isFlat(indexAngle) && isClosed(otherDigitsAngle))
-            {
-                return GRIP_POINTER;
-            }
-            if (isCurled(thumbAngle) && isCurled(indexAngle) && isCurled(otherDigitsAngle))
-            {
-                return GRIP_TRIPOD;
-            }
-            if (isFlat(thumbAngle) && isFlat(indexAngle) && isFlat(otherDigitsAngle))
-            {
-                return GRIP_MOUSE;
-            }
-        }
-        else if (isClosed(wristFlexAngle))
-        {
-            if (isFlat(thumbAngle) && isFlat(indexAngle) && isFlat(otherDigitsAngle))
-            {
-                return GRIP_FLAT;
-            }
-        }
-    }
-    return GRIP_NONE;
+    return grip;
 };
